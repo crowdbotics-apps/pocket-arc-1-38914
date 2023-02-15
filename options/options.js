5dcff2d4f9ca08fa432b924704fe32ad3cbd7bdd
Kyle VanBibber <kylevanbib@gmail.com>
2023-02-15 11:09:37 -0600

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "pocket-arc-1-38914",
   url: "https://pocket-arc-1-38914.botics.co",
   api: "https://pocket-arc-1-38914.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
